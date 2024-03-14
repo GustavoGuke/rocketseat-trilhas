bb9476cb5afcbdbb9128da90f7d25bdcd87ea1ed
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-03-14 06:33:20 -0300

    tema provider

--- reactnative/team/App.tsx
+++ reactnative/team/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'react-native';
 import theme from '@theme/index';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
--- reactnative/team/src/screens/Groups/styles.ts
+++ reactnative/team/src/screens/Groups/styles.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components/native";
 
 
-
 export const Container = styled.View`
     flex: 1;
-    background-color: ${(props: { theme: { COLORS: { GRAY_700: any; }; }; }) => props.theme.COLORS.GRAY_700};
+    background-color: ${({theme}) => theme.COLORS.GRAY_700};
     padding: 24px;
 `
