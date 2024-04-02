1c51fd0dc6c838c08d741bd7d6951a88a7730668
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2024-04-02 10:24:49 -0300

    exclusão projeto em gluestack

--- reactnative/gym/src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner, Center } from "@gluestack-ui/themed";
-
-export function Loading() {
-    return (
-        <Center flex={1} >
-            <Spinner color={"$indigo600"}/>
-        </Center>
-    )
-}
