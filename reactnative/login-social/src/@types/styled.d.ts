beddd3b02d3f0e206be1f561d46fe2fef5b8e681
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-04-27 14:07:32 -0300

    componente Loading com styled-component

---
+++ reactnative/login-social/src/@types/styled.d.ts
@@ -0,0 +1,6 @@
+import 'styled-components/native'
+
+declare module 'styled-components/native' {
+    type ThemeType = typeof theme;
+    export interface DefaultTheme extends ThemeType {}
+}
--- reactnative/login-social/src/screens/Signin/index.tsx
+++ reactnative/login-social/src/screens/Signin/index.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 import { styles } from './style';
@@ -6,7 +6,6 @@ export function Signin() {
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
         </View>
     );
 }
