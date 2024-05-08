ad26240bb075f6958df949525fc4272ea8c2ce73
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-08 07:10:25 -0300

    configuração jest e primeiro teste

--- reactnative/testes/iweather-main/src/utils/getNextDays.test.ts
+++ reactnative/testes/iweather-main/src/utils/getNextDays.test.ts
@@ -1,3 +1,7 @@
-test('', () => {
-    
+import { getNextDays } from "./getNextDays";
+test('should be return the next five days', () => {
+    const days = getNextDays();
+    //console.log(days);
+
+    expect(days.length).toBe(5);
 });
