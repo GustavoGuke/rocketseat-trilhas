597d3348c2afb0d7b8a6693bfe1827345bb57bd6
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-11 17:21:50 -0300

    teste de componente

--- reactnative/testes/iweather-main/src/components/Input/input.test.tsx
+++ reactnative/testes/iweather-main/src/components/Input/input.test.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react-native'
+import { render, screen } from '@testing-library/react-native'
 
 import { Input } from '@components/Input'
 
 describe("Component Input", () => {
-    it("should be render without activity indicator", () => {
-        const { debug } = render(<Input />)
-        debug()
+    it("should be render without activity indicator if is loading prop undefined", () => {
+        render(<Input />)
+
+        const activitindicator = screen.queryByTestId("activity-indicator")
+        expect(activitindicator).toBeNull()
+        //console.log(activitindicator)
+    })
+
+    it("should be render without activity indicator if is loading prop true", () => {
+        render(<Input isLoading/>)
+
+        const activitindicator = screen.queryByTestId("activity-indicator")
+        expect(activitindicator).toBeTruthy()
+        //console.log(activitindicator)
     })
 })
