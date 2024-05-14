669bdb7783ac169c5ab07bf2beb093836f6d5f30
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-14 04:36:49 -0300

    teste mocks metodos do asyncStorage

--- reactnative/testes/iweather-main/src/libs/asyncStorage/cityStorage.test.ts
+++ reactnative/testes/iweather-main/src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,32 @@
-import { getStorageCity } from "./cityStorage";
+import { CityProps } from "@services/getCityByNameService";
+import { getStorageCity, removeStorageCity, saveStorageCity } from "./cityStorage";
 
+
+const newCity: CityProps = {
+    id: '1',
+    latitude: 123,
+    longitude: 456,
+    name: 'São paulo'
+}
 describe("Storage: CityStorage", () => {
     it("should be return null when don´t have a city storaged", async () => {
         const response = await getStorageCity()
-        console.log(response)
+        expect(response).toBeNull()
+        //console.log(response)
+    })
+
+    it("should be return city storaged", async () => {
+        await saveStorageCity(newCity)
+        const response = await getStorageCity()
+
+        expect(response).toEqual(newCity)
+    })
+
+    it("should be remove city storage", async () => {
+        await saveStorageCity(newCity)
+        await removeStorageCity()
+        const response = await getStorageCity()
+
+        expect(response).toBeNull()
     })
 })
