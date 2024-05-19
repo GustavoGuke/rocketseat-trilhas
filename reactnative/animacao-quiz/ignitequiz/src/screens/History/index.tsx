0c722cd783b37017c882c7bbcae222ed686c47ed
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-19 08:59:31 -0300

    animação de Layout

--- reactnative/animacao-quiz/ignitequiz/src/screens/History/index.tsx
+++ reactnative/animacao-quiz/ignitequiz/src/screens/History/index.tsx
@@ -9,6 +9,13 @@ import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
 import { styles } from './styles';
 import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
 import { Loading } from '../../components/Loading';
+import Animated, { 
+  BounceInLeft, 
+  BounceInRight, 
+  SlideInRight, 
+  SlideOutRight,
+  LinearTransition
+} from 'react-native-reanimated';
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
@@ -65,12 +72,18 @@ export function History() {
       >
         {
           history.map((item) => (
-            <TouchableOpacity
+            <Animated.View
               key={item.id}
-              onPress={() => handleRemove(item.id)}
+              entering={SlideInRight}
+              exiting={SlideOutRight}
+              layout={LinearTransition.springify()}
             >
-              <HistoryCard data={item} />
-            </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleRemove(item.id)}
+              >
+                <HistoryCard data={item} />
+              </TouchableOpacity>
+            </Animated.View>
           ))
         }
       </ScrollView>
