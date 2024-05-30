e16d0927ba400812f6b27c38ac5cbe681724aa77
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-30 08:43:28 -0300

    instalando Skia e usando no comp overlay dentro de quiz

--- reactnative/animacao-quiz/ignitequiz/src/screens/Quiz/index.tsx
+++ reactnative/animacao-quiz/ignitequiz/src/screens/Quiz/index.tsx
@@ -17,6 +17,7 @@ import Animated, { interpolate, Easing, interpolateColor, useAnimatedStyle, useS
 import { ProgressBar } from '../../components/ProgressBar';
 import { THEME } from '../../styles/theme';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { OverlayFeedback } from '../../components/OverlayFeedback';
 
 interface Params {
   id: string;
@@ -30,6 +31,7 @@ export function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+  const [statusReply, setStatusReply] = useState(0)
 
   const { navigate } = useNavigation();
 
@@ -101,8 +103,11 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1)
       setPoints(prevState => prevState + 1);
+      
     } else {
+      setStatusReply(2)
       animatedShake();
     }
 
@@ -202,7 +207,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-
+      <OverlayFeedback status={statusReply}/>
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>
           {quiz.title}
