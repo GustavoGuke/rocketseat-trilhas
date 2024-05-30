e65d29364d69aefc8a6f58d2b96d398289d49c96
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-05-30 09:44:48 -0300

    svg trofeu

--- reactnative/animacao-quiz/ignitequiz/src/components/Option/index.tsx
+++ reactnative/animacao-quiz/ignitequiz/src/components/Option/index.tsx
@@ -2,12 +2,35 @@ import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import { styles } from './styles';
 
+import { BlurMask, Canvas, Path, Skia} from '@shopify/react-native-skia';
+import { THEME } from '../../styles/theme';
+import { useEffect, useState } from 'react';
+import { useSharedValue, withTiming } from 'react-native-reanimated';
+
 type Props = TouchableOpacityProps & {
   checked: boolean;
   title: string;
 }
 
+const CHECK_SIZE = 28
+const CHECK_STROKE = 2
+const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2
+
+
 export function Option({ checked, title, ...rest }: Props) {
+  const percentage = useSharedValue(0)
+  const path = Skia.Path.Make()
+  path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
+
+  useEffect(() => {
+    if(checked){
+      percentage.value = withTiming(1, {duration:700})
+    } else {
+      percentage.value = withTiming(0, { duration: 700 })
+    }
+
+  },[checked])
   return (
     <TouchableOpacity
       style={
@@ -21,6 +44,28 @@ export function Option({ checked, title, ...rest }: Props) {
       <Text style={styles.title}>
         {title}
       </Text>
+
+      <Canvas style={{ height: CHECK_SIZE * 2, width: CHECK_SIZE * 2 }}>
+        <Path
+          path={path}
+          color={THEME.COLORS.GREY_300}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+        />
+
+        <Path
+          path={path}
+          color={THEME.COLORS.BRAND_LIGHT}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+          start={0}
+          end={percentage}
+        >
+
+          <BlurMask blur={2} style="solid"/>
+        </Path>
+        
+      </Canvas>
     </TouchableOpacity>
   );
 }
