8e0f65932ad62f4c7c040a1b58230a7287d03d84
gusta <gustavo.athenas13@gmail.com>
2024-08-06 04:56:41 -0300

    mostrando dados sincronizados

--- reactnative/login-social/src/screens/Home/index.tsx
+++ reactnative/login-social/src/screens/Home/index.tsx
@@ -1,20 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
-
+import { CloudArrowUp } from 'phosphor-react-native';
 import { useUser } from '@realm/react';
 import { useQuery, useRealm } from '../../libs/realm';
 import { Historic } from "../../libs/realm/schemas/historic";
 
 import { CarStatus } from "../../components/Card";
 import { HomeHeader } from "../../components/HomeHeader";
+import { TopMessage } from '../../components/TopMessage';
+import { HistoricCard, HistoricCardProps } from "../../components/HistoricCard";
+
 import { Container, Content, Label, Title } from "./styles";
 import { useEffect, useState } from "react";
 import { Alert, FlatList, Text } from "react-native";
-import { HistoricCard, HistoricCardProps } from "../../components/HistoricCard";
 import dayjs from "dayjs";
+import { getLastAsyncTimestamp, saveLastSyncTimestamp } from "../../libs/asyncStorage/syncStorage";
+import Toast from "react-native-toast-message";
 
 export function Home() {
     const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
     const [historicList, setHistoricList] = useState<HistoricCardProps[]>([])
+    const [percetageToSync, setPercentageToSync] = useState<string | null>(null);
     const { navigate } = useNavigation();
     const historic = useQuery(Historic);
     const realm = useRealm();
@@ -36,16 +41,18 @@ export function Home() {
         }
     }
 
-    function fetchHistoric() {
+    async function fetchHistoric() {
 
         try {
             const responde = historic.filtered("status = 'realizada' SORT(created_at DESC)");
+
+            const lastSync = await getLastAsyncTimestamp();
 
             const formattedHistoric = responde.map((historic) => {
                 return ({
                     id: historic._id.toString(),
                     licensePlate: historic.license_plate,
-                    isSync: false,
+                    isSync: lastSync > historic.updated_at!.getTime(),
                     created: dayjs(historic.created_at).format("DD/MM/YYYY HH:mm"),
                 })
             })
@@ -63,6 +70,25 @@ export function Home() {
     useEffect(() => {
         fetchVehicleInUse()
     }, [])
+
+    async function progressNotification(transferred: number, transferable: number) {
+        const percentage = (transferred / transferable) * 100;
+
+        if (percentage === 100) {
+            await saveLastSyncTimestamp();
+            await fetchHistoric();
+            setPercentageToSync(null);
+
+            Toast.show({
+                type: 'info',
+                text1: 'Todos os dados estão sincronizado.'
+            })
+        }
+
+        if (percentage < 100) {
+            setPercentageToSync(`${percentage.toFixed(0)}% sincronizado.`)
+        }
+    }
 
     useEffect(() => {
         realm.addListener('change', () => fetchVehicleInUse())
@@ -85,8 +111,30 @@ export function Home() {
         })
     }, [realm]);
 
+    useEffect(() => {
+        const syncSession = realm.syncSession;
+
+        if (!syncSession) {
+            return;
+        }
+
+        syncSession.addProgressNotification(
+            Realm.ProgressDirection.Upload,
+            Realm.ProgressMode.ReportIndefinitely,
+            progressNotification
+        )
+
+        return () => {
+            syncSession.removeProgressNotification(progressNotification);
+        }
+    }, []);
+
     return (
         <Container>
+
+            {
+                percetageToSync && <TopMessage title={percetageToSync} icon={CloudArrowUp} />
+            }
             <HomeHeader />
             <Content>
                 <CarStatus
