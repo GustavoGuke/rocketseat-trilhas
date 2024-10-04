68d50f9ba07ca3fd2673bd4564ea3792e93afecd
gusta <gustavo.athenas13@gmail.com>
2024-10-04 06:59:27 -0300

    config swagger e seed

--- cursos/saas/saas-rbac/apps/api/src/http/routes/auth/create-account.ts
+++ cursos/saas/saas-rbac/apps/api/src/http/routes/auth/create-account.ts
@@ -1,12 +1,17 @@
+import { hash } from "bcryptjs";
 import { type FastifyInstance } from "fastify";
 import { type ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { prisma } from "../../../lib/prisma";
+
 
 
 export async function createAccount(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/users',
         {
             schema: {
+                tags: ['Auth'],
+                summary: 'Create an account',
                 body: z.object({
                     name: z.string(),
                     email: z.string().email(),
@@ -15,8 +20,33 @@ export async function createAccount(app: FastifyInstance) {
             },
 
         },
-        () => {
-            return 'user created'
+        async (request, reply) => {
+            const { name, email, password } = request.body
+
+            const userWithSameEmail = await prisma.user.findUnique({
+                where: {email}
+            })
+
+            if (userWithSameEmail) {
+                return reply
+                .status(400)
+                .send({
+                    message: 'User already exists.'
+                })
+            }
+
+            const passwordHash = await hash(password, 6)
+
+            await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    passwordHash: passwordHash
+                }
+            })
+
+            return reply.status(201).send()
+
         }
     )
 }
