b7f1dc2e2ba61d3f393b5bdd1bfcdfb34029d09f
gusta <gustavo.athenas13@gmail.com>
2025-01-08 06:40:25 -0300

    componentes da landing page

--- cursos/micro-saas/projectinbio/app/page.tsx
+++ cursos/micro-saas/projectinbio/app/page.tsx
@@ -1,5 +1,8 @@
+import FAQ from "./components/landing-page/Faq";
 import Header from "./components/landing-page/Headers";
 import Hero from "./components/landing-page/Hero";
+import Pricing from "./components/landing-page/pricing";
+import VideoExplanation from "./components/landing-page/Video";
 
 
 export default function Home() {
@@ -11,6 +14,9 @@ export default function Home() {
         <FAQ /> */}
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
 
     </div>
   );
