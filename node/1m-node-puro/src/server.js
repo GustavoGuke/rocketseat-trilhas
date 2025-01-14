2726b6518a843f28508ce1a0f98523344b86a4b7
Gustavo <gustavo.athenas13@gmail.com>
2025-01-14 14:29:53 -0300

    streams - middewares

--- node/1m-node-puro/src/server.js
+++ node/1m-node-puro/src/server.js
@@ -1,8 +1,30 @@
 import http from "node:http";
+import { json } from "./middewares/json.js";
+import { Database } from "./database.js";
 
+const database = new Database
 
-const server = http.createServer((req, res) => {
-    return res.end("hello word - teste")
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { id, name, email } = req.body
+
+    const user = { id, name, email }
+
+    database.insert('users', user)
+
+    return res.end('Criação de usuário')
+  }
 })
 
 server.listen(3333)
+
