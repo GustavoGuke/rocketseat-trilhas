d219e7db76dcdd1502a6fdeaa334263265a97d4d
Gustavo <gustavo.athenas13@gmail.com>
2025-01-27 17:19:29 -0300

    variaveis de ambiente

--- node/2m-fastify/src/server.ts
+++ node/2m-fastify/src/server.ts
@@ -1,10 +1,15 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const tables = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'transação de teste',
+    amount: 1000,
+  })
   return tables
 })
 
