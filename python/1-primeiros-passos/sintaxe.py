e5b858e0451d51552d230ac74e0020fae013bd57
gusta <gustavo.athenas13@gmail.com>
2025-05-17 20:26:31 -0300

    inicio curso python

---
+++ python/1-primeiros-passos/sintaxe.py
@@ -0,0 +1,21 @@
+# print("ola mundo")
+"""
+Comentarios de multiplas linhas
+"""
+print("ola mundo")
+
+# bloco
+idade = 18
+#idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("maior de idade")
+else:
+    print("menor de idade")
+
+
+# while
+contado = 0
+while contado < 5:
+    print(contado)
+    contado += 1
