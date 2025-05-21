23806111aebd189855c4f0cab8866f0186ead574
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2025-05-21 16:44:01 -0300

    fim do projeto - crud completo

--- python/1-primeiros-passos/projeto/gerenciador.py
+++ python/1-primeiros-passos/projeto/gerenciador.py
@@ -4,21 +4,43 @@ def adicionar(tarefas, nome_tarefa):
     tarefas.append(tarefa)
     print("Tarefa adicionada com sucesso: ", nome_tarefa)
     return
+
+
+
 def listar(tarefas):
     for indice, tarefa in enumerate(tarefas, start=1):
         status = "Concluida" if tarefa['concluida'] else ""
         nome_tarefa = tarefa["nome"]
         print(f"{indice} - {nome_tarefa} - [{status}]")
 
-def atualizar(tarefas, nome_tarefa):
-    for tarefa in tarefas:
-        if tarefa['nome'] == nome_tarefa:
-            tarefa['concluida'] = True
-            print("Tarefa concluida com sucesso: ", nome_tarefa)
-            return
-    print("Tarefa nao encontrada: ", nome_tarefa)
+
+def atualizar(tarefas, indice, novo_nome_tarefa):
+    indice_correto = indice_tarefa - 1
+    if indice_correto >= 0 and indice_correto < len(tarefas):
+        tarefas[indice_correto]["nome"] = novo_nome_tarefa
+        print(f"Tarefa:{indice} :  {novo_nome_tarefa} atualizada")
+    else:
+        print("indice não encontrado")
+
+
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_correto = indice_tarefa - 1
+    if indice_correto >= 0 and indice_correto < len(tarefas):
+        tarefas[indice_correto]["concluida"] = True
+        print(f"Tarefa:{indice_correto} :  {tarefas[indice_correto]["nome"]} atualizada")
+    else:
+        print("indice não encontrado")
     return
     
+
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["concluida"]:
+            tarefas.remove(tarefa)
+    print("tarefas deletadas")
+    return
+
+
 while True:
     print("\nMenu\n1 - Adicionar\n2 - Listar\n3 - Atualizar\n4 - Completar tarefa \n5 - Excluir\n6 - Sair")
     opcao = int(input("Digite uma opcao: "))
@@ -29,12 +51,17 @@ while True:
     elif opcao == 2:
         listar(tarefas)
     elif opcao == 3:
-        nome_tarefa = input("Digite o nome da tarefa: ")
-        atualizar(tarefas, nome_tarefa)
+        listar(tarefas)
+        indice_tarefa = int(input("Digite o número da terefa: "))
+        nova_tarefa = input("Digite o novo nome da terefa: ")
+        atualizar(tarefas, indice_tarefa, nova_tarefa)
     elif opcao == 4:
-        print("Completar tarefa")
+        listar(tarefas)
+        indice_tarefa = int(input("Digite o número da terefa: "))
+        completar_tarefa(tarefas, indice_tarefa)
     elif opcao == 5:
-        print("Excluir")
+        deletar_tarefas_completadas(tarefas)
+        listar(tarefas)
     elif opcao == 6:
         print("Sair")
         break
