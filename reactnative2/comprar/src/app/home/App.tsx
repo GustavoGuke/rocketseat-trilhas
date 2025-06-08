94bcab208871c8d88ce1609e5dd4b7f3a6799054
gusta <gustavo.athenas13@gmail.com>
2025-06-08 09:11:02 -0300

    react-native2 - projeto comprar

---
+++ reactnative2/comprar/src/app/home/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, Text, View } from 'react-native';
+import { styles } from './style';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../../assets/logo.png')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
