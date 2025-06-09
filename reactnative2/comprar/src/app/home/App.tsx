206b0c5026ad86558e365eb1ad4103122d3fbd8b
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2025-06-09 13:38:52 -0300

    componente Input and Content

--- reactnative2/comprar/src/app/home/App.tsx
+++ reactnative2/comprar/src/app/home/App.tsx
@@ -2,13 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, Text, View } from 'react-native';
 import { styles } from './style';
 import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Image style={styles.logo} source={require('@/assets/logo.png')} />
-      <Button title='Adicionar'/>
+      <View style={styles.form}>
+        <Input placeholder='O que você precisa comprar' />
+        <Button title='Adicionar' />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
--- reactnative2/comprar/src/app/home/style.ts
+++ reactnative2/comprar/src/app/home/style.ts
@@ -3,14 +3,29 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'gray',
         alignItems: 'center',
         paddingTop: 62,
-        padding: 24
+        
     },
     logo: {
         width: 134,
         height: 34
+    },
+    form: {
+        width: '100%',
+        padding: 24,
+        gap: 7,
+        marginTop: 32
+    },
+    content: {
+        flex:1,
+        width: '100%',
+        backgroundColor: 'white',
+        borderTopLeftRadius:24,
+        borderTopRightRadius:24,
+        padding:24,
+        marginTop: 24
     }
 });
 
