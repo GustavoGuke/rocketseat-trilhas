14f60a2babaafc3528f7672efb377555c973c48f
gusta <gustavo.athenas13@gmail.com>
2025-08-21 22:35:16 -0300

    next-pagerouter

---
+++ next/page-router/src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header"
+
+export default function Blog(){
+    return (
+        <>
+            <Header/>
+            <h2>blog</h2>
+            <h2>blog</h2>
+            <h2>blog</h2>
+        </>
+    )
+}
