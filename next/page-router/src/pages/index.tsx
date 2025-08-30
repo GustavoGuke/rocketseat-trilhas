91109aac76eb599f25d7e2ced01d1e9737d12e90
gusta <gustavo.athenas13@gmail.com>
2025-08-30 10:11:20 -0300

    Secao Hero

--- next/page-router/src/pages/index.tsx
+++ next/page-router/src/pages/index.tsx
@@ -1,18 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { HeroSection } from "@/components/Hero-section";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center dark">
-    </div>
+    <article className="flex flex-col">
+      <HeroSection />
+    </article>
   );
 }
