c4a8a5ffeeaabb70ed00fe34d953f5e46e011b2e
gusta <gustavo.athenas13@gmail.com>
2025-08-31 09:22:44 -0300

    call-to-action section

--- next/page-router/src/components/ui/button.tsx
+++ next/page-router/src/components/ui/button.tsx
@@ -15,6 +15,7 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-gray-400 bg-gray-700 transition-colors duration-200 shadow-sm hover:border-blue-200 hover:text-blue-200",
+        primary: 'bg-blue-200 text-white hover:bg-blue-100 rounded-full',
         secondary:
           "bg-gray-100 text-gray-800 hover:bg-blue-100 rounded-full shadow-sm",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -36,7 +37,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
--- next/page-router/src/pages/index.tsx
+++ next/page-router/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { CallToAction } from "@/components/Call-to-action";
+import { CustomerStorySection } from "@/components/Customer-story-section/Customer-story-section";
 import { FeatureSection } from "@/components/Feature-section";
 import { HeroSection } from "@/components/Hero-section";
 import { SupportSection } from "@/components/Suppport-section";
@@ -9,6 +11,8 @@ export default function Home() {
       <HeroSection />
       <FeatureSection />
       <SupportSection />
+      <CustomerStorySection />
+      <CallToAction />
     </article>
   );
 }
